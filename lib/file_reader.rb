1ad938e711abd9d69254bb286c6556c83272b2e5
vicmaburrito <vmaldaraca@gmail.com>
2021-05-15 15:40:46 -0500

    Adding empty file detector

--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -5,7 +5,8 @@ class FileReader
     @file_path = file_path
     @lines = File.readlines(@file_path)
     @line_number = @lines.size
-    puts "\nFile read sucessfully\n.\n.\n.\n.".colorize(:green)
+    puts "\nFile read sucessfully\n.\n.\n.".colorize(:green)
+    puts file_path.to_s.colorize(color: :white, background: :red) + ' ' + 'is an empty file' if File.empty?(file_path)
   rescue StandardError => e
     @lines = []
     puts "\nThere's a problem with the path you provide #{e}".colorize(:red)
