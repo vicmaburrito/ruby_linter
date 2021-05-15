360f9ce4b4b73cf678f4a544c2eaba45e56a5b85
vicmaburrito <vmaldaraca@gmail.com>
2021-05-15 15:22:34 -0500

    Adding rspec test_files

---
+++ spec/test_files.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/test_files'
+
+describe TestingFiles do
+  path = File.absolute_path('../ruby_linter/good_code.rb')
+  msg_array = TestingFiles.new(path)
+  describe 'results' do
+    it 'it would expect a file name and return an array with all the content of the file' do
+      expect(msg_array.results).to be_an(array)
+    end
+  end
+end
