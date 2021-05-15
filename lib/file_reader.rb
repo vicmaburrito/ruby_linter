44258c8528dd5d919afae4573c2c324b3de93f75
vicmaburrito <vmaldaraca@gmail.com>
2021-05-14 22:15:52 -0500

    Adding files to fix rubocoperros

--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -1,10 +1,9 @@
 class ReadFile
-    attr_reader :file_path, :lines, :line_size
-  
-    def initialize(file_path)
-      @file_path = file_path
-      @lines = File.readlines(@file_path)
-      puts "File_read"
-    end
-end
+  attr_reader :file_path, :lines, :line_size
 
+  def initialize(file_path)
+    @file_path = file_path
+    @lines = File.readlines(@file_path)
+    puts 'File_read'
+  end
+end
