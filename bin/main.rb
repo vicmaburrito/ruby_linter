6b1be0d600b51f273499a1abf143aeb36f15296e
vicmaburrito <vmaldaraca@gmail.com>
2021-05-16 13:22:37 -0500

    Fixing linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -5,8 +5,8 @@ msg = TestingFiles.new(ARGV.first)
 msg.results
 if !msg.errors.empty?
   puts msg.errors
-  puts msg.line_number.to_s + ' lines of code inspected'.colorize(:yellow) + ', ' + msg.errors_number.to_s + ' ' +
-       'offenses'.colorize(:red) + ' ' + 'detected'
+  puts msg.line_number.to_s + ' lines of code inspected, '.colorize(:yellow) + " #{msg.errors_number}" +
+       ' offenses'.colorize(:red) + ' detected'
 else
-  puts ".\nno" + ' ' + 'offenses'.colorize(:green) + ' ' + 'detected'
+  puts ".\nno" + ' offenses'.colorize(:green) + ' detected'
 end
