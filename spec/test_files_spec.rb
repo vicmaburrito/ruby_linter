797e646e47dcf1a2942f0a2cbceadfdcc735b3c1
vicmaburrito <vmaldaraca@gmail.com>
2021-05-17 14:38:28 -0500

    Modified path of test_files_spec

--- spec/test_files_spec.rb
+++ spec/test_files_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/test_files'
 
 describe TestingFiles do
-  path = File.absolute_path('../ruby_linter/good_code.rb')
+  path = File.absolute_path('../ruby_linter/Example_files/good_code.rb')
   msg_array = TestingFiles.new(path)
   describe '#results' do
     it 'it would expect a file name and return an array with all the content of the file' do
