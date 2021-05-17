23415da1a258e264f14718f152719954ca696f89
vicmaburrito <vmaldaraca@gmail.com>
2021-05-16 21:08:33 -0500

    adding new test space_methods and update rspec

--- bin/main.rb
+++ bin/main.rb
@@ -5,7 +5,7 @@ msg = TestingFiles.new(ARGV.first)
 msg.results
 if !msg.errors.empty?
   puts msg.errors
-  puts "#{msg.line_number.to_s.uncolorize} #{'lines of code inspected,'.yellow},
+  puts "#{msg.line_number.to_s.uncolorize} #{'lines of code inspected'.yellow}
 #{msg.errors_number.to_s.red} #{'offenses'.red} #{'detected'.uncolorize}"
 else
   puts ".\nno #{'offenses'.green} detected"
--- spec/test_files.rb
+++ spec/test_files.rb
@@ -3,7 +3,7 @@ require_relative '../lib/test_files'
 describe TestingFiles do
   path = File.absolute_path('../ruby_linter/good_code.rb')
   msg_array = TestingFiles.new(path)
-  describe 'results' do
+  describe '#results' do
     it 'it would expect a file name and return an array with all the content of the file' do
       expect(msg_array.results).to be_an(Array)
     end
