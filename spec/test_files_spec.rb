ff15a37d0959daf2644ad5c761e58fd61dc56e78
vicmaburrito <vmaldaraca@gmail.com>
2021-05-18 12:06:03 -0500

    Update rspec

--- spec/test_files_spec.rb
+++ spec/test_files_spec.rb
@@ -1,7 +1,28 @@
 require_relative '../lib/test_files'
+require 'colorize'
 
 describe TestingFiles do
   path = File.absolute_path('../ruby_linter/Example_files/good_code.rb')
+  msg_array = TestingFiles.new(path)
+  describe '#results' do
+    context 'return an array' do
+      it { expect(msg_array.results).not_to be_empty }
+    end
+  end
+end
+
+describe TestingFiles do
+  path = File.absolute_path('../ruby_linter/Example_files/empty_file.rb')
+  msg_array = TestingFiles.new(path)
+  describe '#results' do
+    context 'empty file' do
+      it { expect(msg_array.results).to be_empty }
+    end
+  end
+end
+
+describe TestingFiles do
+  path = File.absolute_path('../ruby_linter/Example_files/bad_code.rb')
   msg_array = TestingFiles.new(path)
   describe '#results' do
     context 'return an array' do
