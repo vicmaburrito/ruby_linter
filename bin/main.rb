581f3a94044a28c392b1bb77e1eb53cb3d397f23
vicmaburrito <vmaldaraca@gmail.com>
2021-05-18 13:02:05 -0500

    Fixing linter

--- bin/main.rb
+++ bin/main.rb
@@ -1,13 +1,12 @@
 #!/usr/bin/env ruby
-require 'colorize'
 require_relative '../lib/test_files'
 
 msg = TestingFiles.new(ARGV.first)
 msg.results
 if !msg.errors.empty?
   puts msg.errors
-  puts "#{msg.line_number.to_s.uncolorize} #{'lines of code inspected'.yellow}
-#{msg.errors_number.to_s.red} #{'offenses'.red} #{'detected'.uncolorize}"
+  puts "#{msg.line_number.to_s} #{'lines of code inspected'}
+#{msg.errors_number.to_s} #{'offenses'} #{'detected'}"
 else
-  puts ".\nno #{'offenses'.green} detected"
+  puts ".\nno #{'offenses'} detected"
 end
