b2fb13cddefda056f74ff1fca0773a7dac03c14a
vicmaburrito <vmaldaraca@gmail.com>
2021-05-18 11:42:03 -0500

    Update rspec

--- spec/test_files_spec.rb
+++ spec/test_files_spec.rb
@@ -10,23 +10,3 @@ describe TestingFiles do
     end
   end
 end
-
-describe TestingFiles do
-  path = File.absolute_path('../ruby_linter/Example_files/empty_file.rb')
-  msg_array = TestingFiles.new(path)
-  describe '#results' do
-    context 'empty file' do
-      it { expect(msg_array.results).to be_empty }
-    end
-  end
-end
-
-describe TestingFiles do
-  path = File.absolute_path('../ruby_linter/Example_files/bad_code.rb')
-  msg_array = TestingFiles.new(path)
-  describe '#results' do
-    context 'return an array' do
-      it { expect(msg_array.results).not_to be_empty }
-    end
-  end
-end
